49db1feedd92c42aa113fc6319f8ed6763edec5a
Aloisius <kalosius43@gmail.com>
2023-04-28 16:11:41 +0300

    ALOISIUS_KASOZI

--- aloisius_kasozi/env/genq/myform/models.py
+++ aloisius_kasozi/env/genq/myform/models.py
@@ -7,7 +7,7 @@ class User_account(models.Model):
     date =  models.DateTimeField(auto_now=True)
     gender = models.CharField(max_length=22)
     country = models.CharField(max_length=30)
-    town = models.IntegerField(max_length=20)
+    town = models.IntegerField()
     state = models.CharField(max_length=30)
     num1 = models.CharField(max_length=30)
     num2 = models.CharField(max_length=30)
