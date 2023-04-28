0c5ebfaed739368ab40d795a511417a1d87f3f4b
keem <hakeem255muts@gmail.com>
2023-04-28 17:46:08 +0300

    dd

---
+++ MUTEBI ABDUL HAKEEM/env/genq/myform/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User_account(models.Model):
+    firstname = models.CharField(max_length=22 )
+    lastname = models.CharField(max_length=6)
+    date =  models.DateTimeField(auto_now=True)
+    gender = models.CharField(max_length=22)
+    country = models.CharField(max_length=30)
+    town = models.IntegerField()
+    state = models.CharField(max_length=30)
+    num1 = models.CharField(max_length=30)
+    num2 = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    
+    
